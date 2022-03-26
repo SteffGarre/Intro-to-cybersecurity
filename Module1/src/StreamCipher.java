64b9a1db96b544e8e62030ac1f4ecf1462f7eeaa
SteffGarre <61310653+SteffGarre@users.noreply.github.com>
2022-03-26 18:37:14 +0100

    added try/catch

--- Module1/src/StreamCipher.java
+++ Module1/src/StreamCipher.java
@@ -1,3 +1,15 @@
 public class StreamCipher {
 
+    public static void main(String[] args) {
+
+        try{
+
+            long key = Long.parseLong(args[0]);
+
+        } catch(Exception e){
+            System.out.println("Exception catched: " + e);
+            System.out.println("System exit! Good bye :(");
+            System.exit(-1);
+        }
+    }
 }
